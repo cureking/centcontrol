4ed2b38eaf3df1e6495d3d88bf52536ae6a9c92e
jarry <15958002454@163.com>
2019-05-09 14:15:12 +0800

    解决了mapper问题，首先是mybatis中路径配置错误，其次是springboot自动配置的connector版本过高，原url地址无法使用

--- src/main/java/com/renewable/centcontrol/common/RabbitMQPool.java
+++ src/main/java/com/renewable/centcontrol/common/RabbitMQPool.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeoutException;
  * @Author: jarry
  */
 @Slf4j
-public class rabbitMQPool {
+public class RabbitMQPool {
 
     private static ConnectionFactory factory;
 
